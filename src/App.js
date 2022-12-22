84008ec611154d8c8230ad5ddbab7dbf9b28816b
AvishaiBrown <avishaib@cloudcase.net>
2022-12-22 19:23:03 +1100

    Initialised firebase and firestore

--- src/App.js
+++ src/App.js
@@ -10,6 +10,43 @@ import { CssBaseline, Container, LinearProgress } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { MENU_ITEMS, NAME, COPYRIGHT } from "./utils/constants";
 import playfairDisplay from "typeface-playfair-display";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const {
+  initializeAppCheck,
+  ReCaptchaV3Provider,
+} = require("firebase/app-check");
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+//TODO: Deploy to Firebase Hosting by running 'firebase login' and 'firebase init' commands
+// Associate app with Firebase project: "firebase user --add"
+//Specify site in firebase.json: { "hosting": { "site": "sybersbooks" } }
+//Deploy "firebase deploy --only hosting:sybersbooks"
+//To run app locally via firebase CLI: "firebase emulators:start --only hosting"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6UFqJ5S5hGJJvjCfy9hxNXSTNUy9w4v8",
+  authDomain: "sybers-books.firebaseapp.com",
+  projectId: "sybers-books",
+  storageBucket: "sybers-books.appspot.com",
+  messagingSenderId: "706386815840",
+  appId: "1:706386815840:web:dc1ae6a53b34cb88ef4c05",
+  measurementId: "G-56S6BK0TZY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6LdMOJcjAAAAANU2m3jcWbDF_DNmZbLK8OgIwswB"),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true,
+});
 
 const theme = createTheme({
   palette: {
