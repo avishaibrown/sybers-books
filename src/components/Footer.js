069ab56298a732411e3bbd581dac74d983506e3c
AvishaiBrown <avishaib@cloudcase.net>
2023-02-07 15:23:15 +1100

    Added Facebook Icon to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,37 @@
 import * as React from "react";
-import { Box } from "@mui/material";
+import { Box, Stack, IconButton } from "@mui/material";
 import Typography from "./Typography";
+import { Facebook } from "@mui/icons-material";
 
 const Footer = (props) => {
   const { image, imageAlt, copyright } = props;
 
   return (
     <Box component="footer" sx={{ py: 4, textAlign: "center" }}>
-      <Box
-        component="img"
-        alt={imageAlt}
-        src={image}
-        sx={{
-          maxHeight: { xs: 250, md: 400 },
-          maxWidth: { xs: 150, md: 240 },
-        }}
+      <Stack
+        direction="row"
+        spacing={{ xs: 2, md: 4 }}
+        justifyContent="center"
         mb={2}
-      />
+      >
+        <Box
+          component="img"
+          alt={imageAlt}
+          src={image}
+          sx={{
+            maxHeight: { xs: 240, md: 360 },
+            maxWidth: { xs: 200, md: 220 },
+            my: 1,
+          }}
+        />
+        <IconButton
+          href="https://www.facebook.com/SybersBooks/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Facebook />
+        </IconButton>
+      </Stack>
       <Typography variant="body2" color="text.secondary" align="center">
         {copyright} {new Date().getFullYear()}. All rights reserved.
       </Typography>
